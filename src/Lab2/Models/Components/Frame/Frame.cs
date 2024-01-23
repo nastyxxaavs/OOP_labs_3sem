3653c36a79ee777ae1460cb1f67f53934ff3a6db
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:31:46 +0300

    Create Frame.cs

---
+++ src/Lab2/Models/Components/Frame/Frame.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites;
+
+public class Frame
+{
+    public DimensionsOfFrame? Dimensions { get; set; }
+    public DimensionsOfMotherBoard? CorrectMotherBoard { get; set; }
+    public VideoCardOpportunity? VideoCard { get; set; }
+}
