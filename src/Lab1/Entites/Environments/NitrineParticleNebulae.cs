3d80821bd3125a3941f51c49fe9d36f4e25174ac
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 14:56:45 +0300

    Create NitrineParticleNebulae.cs

---
+++ src/Lab1/Entites/Environments/NitrineParticleNebulae.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Itmo.ObjectOrientedProgramming.Lab1.Entites.Obstacles;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Entites.Enviroments;
+
+public class NitrineParticleNebulae : EnvironmentBase
+{
+    public NitrineParticleNebulae()
+    {
+        Obstacles = new List<Obstacles.ObstacleBase>() { new None(), new SpaceWhales() };
+    }
+}
