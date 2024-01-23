3cb78a3df5e4fc2f7e6456f6ff68d02c96324299
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:52:09 +0300

    Create Message.cs

---
+++ src/Lab3/Message/Message.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab3.Message;
+
+public class Message
+{
+    public string? Title { get; set; }
+    public string? Body { get; set; }
+    public int? ImportanceLevel { get; set; }
+}
