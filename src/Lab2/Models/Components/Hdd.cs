695c18a7b00f50208f90db3dae96a995b440bc3b
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:27:19 +0300

    Create Hdd.cs

---
+++ src/Lab2/Models/Components/Hdd.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites;
+
+public class Hdd
+{
+    public double Capacity { get; set; }
+    public double SpindleSpeed { get; set; }
+    public double SpendedPower { get; set; }
+}
