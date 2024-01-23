ef0c4cf609b5fa449d671cc24d5c5df7871f38ac
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:26:16 +0300

    Create Wifi.cs

---
+++ src/Lab2/Models/Components/Wifi.cs
@@ -0,0 +1,9 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites;
+
+public class Wifi
+{
+    public string? StandartVersion { get; set; }
+    public bool Bluetooth { get; set; }
+    public string? PciVersion { get; set; }
+    public double SpendedPower { get; set; }
+}
