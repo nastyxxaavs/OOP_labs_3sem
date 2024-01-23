9eac6ba69a70e41090e29e69b7eac9e054cf1bd7
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:32:50 +0300

    Create DimensionsOfCoolingSystem.cs

---
+++ src/Lab2/Models/Components/CoolingSystem/DimensionsOfCoolingSystem.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites.Components;
+
+public record DimensionsOfCoolingSystem()
+{
+    public double Height { get; init; }
+    public double Width { get; init; }
+    public double Length { get; init; }
+}
