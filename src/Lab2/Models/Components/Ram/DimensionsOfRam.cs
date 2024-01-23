b7179c92e1b068f2a4204302e1f35d45ca4a7eca
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:29:20 +0300

    Create DimensionsOfRam.cs

---
+++ src/Lab2/Models/Components/Ram/DimensionsOfRam.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites;
+
+public record DimensionsOfRam()
+{
+    public double Length { get; init; }
+    public double Width { get; init; }
+    public double Height { get; init; }
+}
