8ecff56ddaed43722073cee7707c81f228aa3c0d
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:31:27 +0300

    Create DimensionsOfMotherBoard.cs

---
+++ src/Lab2/Models/Components/Frame/DimensionsOfMotherBoard.cs
@@ -0,0 +1,8 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites;
+
+public record DimensionsOfMotherBoard()
+{
+    public double Length { get; init; }
+    public double Width { get; init; }
+    public double Height { get; init; }
+}
