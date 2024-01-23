05b1ace362eee6eccaad64bda60e213e104d3e90
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-01-23 15:25:59 +0300

    Create XmpProfile.cs

---
+++ src/Lab2/Models/Components/XmpProfile.cs
@@ -0,0 +1,9 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2.Entites;
+
+public class XmpProfile
+{
+    public string? Name { get; set; }
+    public string? Timings { get; set; }
+    public double Voltage { get; set; }
+    public double Frequency { get; set; }
+}
